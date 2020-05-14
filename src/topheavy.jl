8bc17923f2ca7c96a411d2a37460e386bfce8015
Kevin Cazelles <kcazelle@uoguelph.ca>
2020-05-14 16:43:03 -0400

    :sparkles: add topheavy()

---
+++ src/topheavy.jl
@@ -0,0 +1,20 @@
+"""
+...
+# Conpute the average over time of four biomass ratios
+- `sol`: solution of the ODE problem.
+- `burn::Int64 `: number of time steps to be thrown out (burn-in).
+...
+"""
+function topheavy(sol, burn::Int64 = 0)
+    burn += 1 # could be time rather than number of time steps
+    hcat(
+        mean(sol[3, burn:end] ./ (sol[1, burn:end] .+ sol[2, burn:end])),
+        mean(sol[3, burn:end] ./ sol[1, burn:end]),
+        mean(sol[3, burn:end] ./ sol[2, burn:end]),
+        mean(sol[1, burn:end] ./ sol[2, burn:end])
+    )
+end
+"""
+
+
+
