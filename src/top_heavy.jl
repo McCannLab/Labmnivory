8c4bc6b082886fe036d9e397c4cd7f912df4a471
Kevin Cazelles <kcazelle@uoguelph.ca>
2020-05-15 09:37:17 -0400

    constraining type

--- src/top_heavy.jl
+++ src/top_heavy.jl
@@ -7,12 +7,12 @@ using Statistics
 - `burn::Int `: number of time steps to be thrown out (burn-in).
 ...
 """
-function top_heavy(sol, burn::Int = 0)
+function top_heavy(sol::Array{T}, burn::Int = 0)::Array{T} where T
     burn += 1 # could be time rather than number of time steps
     return [
-        mean(sol[3, burn:end] ./ (sol[1, burn:end] .+ sol[2, burn:end]))
-        mean(sol[3, burn:end] ./ sol[1, burn:end])
-        mean(sol[3, burn:end] ./ sol[2, burn:end])
+        mean(sol[3, burn:end] ./ (sol[1, burn:end] .+ sol[2, burn:end])),
+        mean(sol[3, burn:end] ./ sol[1, burn:end]),
+        mean(sol[3, burn:end] ./ sol[2, burn:end]),
         mean(sol[1, burn:end] ./ sol[2, burn:end])
     ]
 end
