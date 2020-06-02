d16e79e052aa012881c301c121a6b0a44f931a00
Gabriel <gabrielgellner@gmail.com>
2020-06-02 09:33:24 -0400

    scale the omnivory between the types of omnivory

--- src/omnivory_time_series.jl
+++ src/omnivory_time_series.jl
@@ -5,11 +5,11 @@ let
     u0 = [1.0, 0.5, 0.1]
     t_span = (0.0, 100.0)
 
-    chain_par = ModelPar(ω = 0.0, a_CP = 0.6, K = 2.0)
+    chain_par = ModelPar(Ω = 0.0, a_CP = 0.6, K = 2.0)
     prob = ODEProblem(model!, u0, t_span, chain_par)
     sol_chain = solve(prob, abstol = 1e-8, reltol = 1e-8)
 
-    omn_par = ModelPar(ω = 0.3, a_CP = 0.6, K = 2.0, pref = adapt_pref)
+    omn_par = ModelPar(Ω = 0.3, a_CP = 0.6, K = 2.0)
     prob = ODEProblem(model!, u0, t_span, omn_par)
     sol_omn = solve(prob, abstol = 1e-8, reltol = 1e-8)
 
