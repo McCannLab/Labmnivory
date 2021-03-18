ce3aa48af6e8bc37303ebc9ae947ee2449a23d1d
Kevin Cazelles <kcazelle@uoguelph.ca>
2021-03-18 10:23:41 -0400

    add pkg to be installed and repro workflow

---
+++ src/install_packages.jl
@@ -0,0 +1,6 @@
+# Here is the code to install packages. 
+# The list below is an example, none of the packages are actually required.
+using Pkg
+Pkg.add(
+    ["Parameters", "RecursiveArrayTools", "Statistics", "DifferentialEquations", "PyPlot", "ForwardDiff", "LinearAlgebra"]
+)
