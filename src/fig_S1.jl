17dfc5e7339854a066c23764b393b116b79bd3ec
chrisgg <chris@greyson-gaito.co.uk>
2022-12-01 15:37:37 -0500

    added parallel to supplementary figures code

--- src/fig_S1.jl
+++ src/fig_S1.jl
@@ -1,48 +1,54 @@
 include("basic_omnivory_module.jl")
 include("pulse.jl")
+using Hwloc
+using .Threads
 using PyPlot
 pygui(true)
 
-# PRESS SIMULATIONS for ranges of K, aCP, eCP and mP.
+# PULSE SIMULATIONS for ranges of K, aCP, eCP and mP.
 
 # range of K
 println("Simulations for a range of K values")
-res_K = []
-rg_K = 2:0.05:4
-for k in rg_K
+rg_K = 2:0.5:4
+# rg_K = 2:0.05:4
+res_K_pulse = Vector{Any}(undef,length(rg_K))
+@threads for i in eachindex(rg_K)
     par = ModelPar()
-    par.K_base = par.K = k
-    res_K = push!(res_K, pulse(par, 0.1, 2.0, 2.0))
+    par.K_base = par.K = rg_K[i]
+    res_K_pulse[i] = pulse(par, 0.1, 2.0, 2.0)
 end 
 
 # range of a_CP
 println("Simulations for a range of aCP values")
-res_aCP = []
-rg_aCP = 0.3:.0025:.55
-for a in rg_aCP
+rg_aCP = 0.3:0.025:0.55
+# rg_aCP = 0.3:0.0025:0.55
+res_aCP_pulse = Vector{Any}(undef,length(rg_aCP))
+@threads for i in eachindex(rg_aCP)
     par = ModelPar()
-    par.a_CP = a
-    res_aCP = push!(res_aCP, pulse(par, 0.1, 2.0, 2.0))
+    par.a_CP = rg_aCP[i]
+    res_aCP_pulse[i] = pulse(par, 0.1, 2.0, 2.0)
 end 
 
 # range of e_CP
 println("Simulations for a range of eCP values")
-res_eCP = []
-rg_eCP = 0.35:.0025:0.7
-for e in rg_eCP
+rg_eCP = 0.35:0.025:0.7
+# rg_eCP = 0.35:.0025:0.7
+res_eCP_pulse = Vector{Any}(undef,length(rg_eCP))
+@threads for i in eachindex(rg_eCP)
     par = ModelPar()
-    par.e_CP = e
-    res_eCP = push!(res_eCP, pulse(par, 0.1, 2.0, 2.0))
+    par.e_CP = rg_eCP[i]
+    res_eCP_pulse[i] = pulse(par, 0.1, 2.0, 2.0)
 end 
 
 # range of m_P
 println("Simulations for a range of mP values")
-res_mP = []
-rg_mP = 0.15:.005:.3
-for m in rg_mP
+rg_mP = 0.15:0.05:0.3
+# rg_mP = 0.15:0.005:0.3
+res_mP_pulse = Vector{Any}(undef,length(rg_mP))
+for i in eachindex(rg_mP)
     par = ModelPar()
-    par.m_P = m
-    res_mP = push!(res_mP, pulse(par, 0.1, 2.0, 2.0))
+    par.m_P = rg_mP[i]
+    res_mP_pulse[i] = pulse(par, 0.1, 2.0, 2.0)
 end 
 
 # As this takes some time to run you may want to save this (it takes ~500Mo), 
@@ -51,10 +57,10 @@ end
 # mkdir("res")
 # save(
 #     "res/res_S1.jld2", 
-#     "rg_K", rg_K, "res_K", res_K,
-#     "rg_aCP", rg_aCP, "res_aCP", res_aCP,
-#     "rg_eCP", rg_eCP, "res_eCP", res_eCP,
-#     "rg_mP", rg_mP, "res_mP", res_mP
+#     "rg_K", rg_K, "res_K_pulse", res_K_pulse,
+#     "rg_aCP", rg_aCP, "res_aCP_pulse", res_aCP_pulse,
+#     "rg_eCP", rg_eCP, "res_eCP_pulse", res_eCP_pulse,
+#     "rg_mP", rg_mP, "res_mP_pulse", res_mP_pulse
 #     )
 
 
@@ -68,32 +74,33 @@ tly = ["-1/Re(λ1)" "Degree of Overshoot" "Max-Min"]
 tlx = ["" "" "K"]
 for i in 1:3
     subplot(3, 4, (i - 1) * 4 + 1)
-    plot_sa_unit(rg_K, res_K, ind[i], false, tlx[i], tly[i])
+    plot_sa_unit(rg_K, res_K_pulse, ind[i], false, tlx[i], tly[i])
 end 
 
 ##----------- aCP
 tlx = ["" "" L"a_{CP}"]
 for i in 1:3
     subplot(3, 4, (i-1) * 4 + 2)
-    plot_sa_unit(rg_aCP, res_aCP, ind[i], false, tlx[i], "")
+    plot_sa_unit(rg_aCP, res_aCP_pulse, ind[i], false, tlx[i], "")
 end 
 
 ##----------- eCP
 tlx = ["" "" L"e_{CP}"]
 for i in 1:3
     subplot(3, 4, (i-1) * 4 + 3)
-    plot_sa_unit(rg_eCP, res_eCP, ind[i], false, tlx[i], "")
+    plot_sa_unit(rg_eCP, res_eCP_pulse, ind[i], false, tlx[i], "")
 end 
 
 ##----------- mP
 tlx = ["" "" L"m_P"]
 for i in 1:3
     subplot(3, 4, (i-1) * 4 + 4)
-    plot_sa_unit_rev(rg_mP, res_mP, ind[i], i == 1, tlx[i], "")
+    plot_sa_unit_rev(rg_mP, res_mP_pulse, ind[i], i == 1, tlx[i], "")
     val = [0.15, 0.20, 0.25, 0.30]
     xticks(val, reverse(val))
 end 
 
 
 tight_layout()
-savefig("figs/figS1_v3.svg")
+savefig("fig/figS1_v3_parallel_reduced.svg")
+# savefig("fig/figS1_v3_parallel_all.svg")
--- src/fig_S2.jl
+++ src/fig_S2.jl
@@ -1,48 +1,54 @@
 include("basic_omnivory_module.jl")
 include("press.jl")
+using Hwloc
+using .Threads
 using PyPlot
 pygui(true)
 
-# PULSE SIMULATIONS for ranges of K, aCP, eCP and mP.
+# PRESS SIMULATIONS for ranges of K, aCP, eCP and mP.
 
 # range of K
 println("Simulations for a range of K values")
-res_K = []
-rg_K = 2:0.05:4
-for k in rg_K
+rg_K = 2:0.5:4
+# rg_K = 2:0.05:4
+res_K_press = Vector{Any}(undef,length(rg_K))
+@threads for i in eachindex(rg_K)
     par = ModelPar()
-    par.K_base = par.K = k
-    res_K = push!(res_K, press(par, 0.1, 0.5))
+    par.K_base = par.K = rg_K[i]
+    res_K_press[i] = press(par, 0.1, 0.5)
 end 
 
 # range of a_CP
 println("Simulations for a range of aCP values")
-res_aCP = []
-rg_aCP = 0.3:.0025:.55
-for a in rg_aCP
+rg_aCP = 0.3:0.025:.55
+# rg_aCP = 0.3:0.0025:.55
+res_aCP_press = Vector{Any}(undef,length(rg_aCP))
+@threads for i in eachindex(rg_aCP)
     par = ModelPar()
-    par.a_CP = a
-    res_aCP = push!(res_aCP, press(par, 0.1, 0.5))
+    par.a_CP = rg_aCP[i]
+    res_aCP_press[i] = press(par, 0.1, 0.5)
 end 
 
 # range of e_CP
 println("Simulations for a range of eCP values")
-res_eCP = []
-rg_eCP = 0.35:.0025:0.7
-for e in rg_eCP
+rg_eCP = 0.35:0.05:0.7
+# rg_eCP = 0.35:.025:0.7
+res_eCP_press = Vector{Any}(undef,length(rg_eCP))
+@threads for i in eachindex(rg_eCP)
     par = ModelPar()
-    par.e_CP = e
-    res_eCP = push!(res_eCP, press(par, 0.1, 0.5))
+    par.e_CP = rg_eCP[i]
+    res_eCP_press[i] = press(par, 0.1, 0.5)
 end 
 
 # range of m_P
 println("Simulations for a range of mP values")
-res_mP = []
-rg_mP = 0.15:.005:.3
-for m in rg_mP
+rg_mP = 0.15:0.05:0.3
+# rg_mP = 0.15:0.005:0.3
+res_mP_press = Vector{Any}(undef,length(rg_mP))
+@threads for i in eachindex(rg_mP)
     par = ModelPar()
-    par.m_P = m
-    res_mP = push!(res_mP, press(par, 0.1, 0.5))
+    par.m_P = rg_mP[i]
+    res_mP_press[i] = press(par, 0.1, 0.5)
 end 
 
 # As this takes some time to run you may want to save this (it takes ~500Mo), 
@@ -51,10 +57,10 @@ end
 # # mkdir("res")
 # save(
 #     "res/res_S2.jld2", 
-#     "rg_K", rg_K, "res_K", res_K,
-#     "rg_aCP", rg_aCP, "res_aCP", res_aCP,
-#     "rg_eCP", rg_eCP, "res_eCP", res_eCP,
-#     "rg_mP", rg_mP, "res_mP", res_mP
+#     "rg_K", rg_K, "res_K_press", res_K_press,
+#     "rg_aCP", rg_aCP, "res_aCP_press", res_aCP_press,
+#     "rg_eCP", rg_eCP, "res_eCP_press", res_eCP,_press
+#     "rg_mP", rg_mP, "res_mP_press", res_mP_press
 #     )
 
 
@@ -69,32 +75,33 @@ tly = ["-1/Re(λ1)" "Degree of Overshoot" "Max-Min"]
 tlx = ["" "" "K"]
 for i in 1:3
     subplot(3, 4, (i - 1) * 4 + 1)
-    plot_sa_unit(rg_K, res_K, ind[i], false, tlx[i], tly[i])
+    plot_sa_unit(rg_K, res_K_press, ind[i], false, tlx[i], tly[i])
 end 
 
 ##----------- aCP
 tlx = ["" "" L"a_{CP}"]
 for i in 1:3
     subplot(3, 4, (i-1) * 4 + 2)
-    plot_sa_unit(rg_aCP, res_aCP, ind[i], false, tlx[i], "")
+    plot_sa_unit(rg_aCP, res_aCP_press, ind[i], false, tlx[i], "")
 end 
 
 ##----------- eCP
 tlx = ["" "" L"e_{CP}"]
 for i in 1:3
     subplot(3, 4, (i-1) * 4 + 3)
-    plot_sa_unit(rg_eCP, res_eCP, ind[i], false, tlx[i], "")
+    plot_sa_unit(rg_eCP, res_eCP_press, ind[i], false, tlx[i], "")
 end 
 
 ##----------- mP
 tlx = ["" "" L"m_P"]
 for i in 1:3
     subplot(3, 4, (i-1) * 4 + 4)
-    plot_sa_unit_rev(rg_mP, res_mP, ind[i], i == 1, tlx[i], "")
+    plot_sa_unit_rev(rg_mP, res_mP_press, ind[i], i == 1, tlx[i], "")
     val = [0.15, 0.20, 0.25, 0.30]
     xticks(val, reverse(val))
 end 
 
 tight_layout()
-savefig("fig/fig_S2.svg")
+savefig("fig/fig_S2_parallel_reduced.svg")
+# savefig("fig/fig_S2_parallel_all.svg")
 
